f9306f9df06c00651f970b13d46f720c7292de79
swapnil patil <swapnil.patil2205@hotmail.com>
2021-12-14 20:25:20 +0530

    completed module one

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,112 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/swapnil.jpg'),
+              ),
+              Text(
+                'Avid Dreamer',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontFamily: 'FuzzyBubbles',
+                  fontWeight: FontWeight.bold
+                ),
+              ),Text(
+                'Flutter developer',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.phone,
+                      color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+91 8728689274',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade900
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ), Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'swapnil@email.com',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.teal.shade900
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,color:Colors.teal.shade900
+                  ),title: Text(
+                  'swapnil@email.comm',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color:Colors.teal.shade100
+                  ),
+                ),
+                ),
+              )
+            ]
+            ,
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
